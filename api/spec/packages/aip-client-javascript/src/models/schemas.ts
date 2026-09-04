55e73e82e3c2d778780af990bfc26bad3f0699d2
Alex Goth <64845621+GAlexIHU@users.noreply.github.com>
2026-09-04 15:41:38 +0200

    feat(ledger): support custom currency balance history

--- api/spec/packages/aip-client-javascript/src/models/schemas.ts
+++ api/spec/packages/aip-client-javascript/src/models/schemas.ts
@@ -3491,6 +3491,7 @@ export const creditAdjustment = z
 export const creditBalance = z
   .object({
     currency: billingCurrencyCode,
+    customCurrencyId: ulid.optional(),
     live: numeric,
     settled: numeric,
     pending: numeric,
@@ -3547,6 +3548,7 @@ export const creditTransaction = z
     bookedAt: dateTime,
     type: creditTransactionType,
     currency: billingCurrencyCode,
+    customCurrencyId: ulid.optional(),
     amount: numeric,
     availableBalance: z
       .object({
@@ -11148,6 +11150,7 @@ export const creditAdjustmentWire = z
 export const creditBalanceWire = z
   .strictObject({
     currency: billingCurrencyCodeWire,
+    custom_currency_id: ulidWire.optional(),
     live: numericWire,
     settled: numericWire,
     pending: numericWire,
@@ -11204,6 +11207,7 @@ export const creditTransactionWire = z
     booked_at: dateTimeWire,
     type: creditTransactionTypeWire,
     currency: billingCurrencyCodeWire,
+    custom_currency_id: ulidWire.optional(),
     amount: numericWire,
     available_balance: z
       .strictObject({
